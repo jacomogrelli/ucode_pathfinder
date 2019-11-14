c88060ccfb2f3a727659c68a48d80bd0acfe7167
Oleksii Nechaiev <onechaiev@e2r12p11.unit.ua>
2019-11-14 16:53:40 +0200

    -

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include "pathfinder.h"
 
-int main () {
-    printf("hello world");
+int main (int argc, char **argv) {
+    char *file_to_str = NULL;
+    char **arr_str = NULL;
+    int islands_count = 0;
+
+    if (argc > 0)
+    file_to_str = mx_file_to_str(argv[1]);
+    arr_str = mx_strsplit(file_to_str, '\n');
+    islands_count = file_to_str[0] - 48;
+    
 }
