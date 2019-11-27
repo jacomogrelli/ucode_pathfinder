881442a742731d2c1218c17282942a0b8ab8c580
Oleksii Nechaiev <onechaiev@e2r4p7.unit.ua>
2019-11-27 15:51:22 +0200

    -

--- pf_uniq_matrix.c
+++
@@ -1,8 +0,0 @@
-#include "pathfinder.h"
-
-char **pf_uniq_matrix(char **matrix, int *isl_count) {
-    char **result = mx_bubble_sort(matrix, 12);
-    if (*isl_count >= 0)
-    mx_print_strarr(result, "__");
-    return result;
-}
--- src/pf_uniq_matrix.c
+++ src/pf_uniq_matrix.c
@@ -1,8 +1,47 @@
 #include "pathfinder.h"
 
 char **pf_uniq_matrix(char **matrix, int *isl_count) {
-    char **result = mx_bubble_sort(matrix, 12);
+    char **buf = matrix;
+    char **res = NULL;
+    int count = 0;
+    int k = 2;
     if (*isl_count >= 0)
-    mx_print_strarr(result, "__");
-    return result;
+    
+    for (; buf[k] != NULL; k += 2) {
+        for (int i = k; buf[i] != NULL; i++) {
+            buf[i] = buf[i + 1];
+            count = i;
+        }
+    }
+    k = mx_bubble_sort(buf, count);
+    for (int i = 0; buf[i + 1] != NULL; i++) {
+        for (int j = i + 1; buf[j] != NULL; j++) {
+            if (mx_strcmp(buf[i], buf[j]) == 0) {
+                buf[i] = "del";
+                i++;
+                count--;
+            }
+        }
+    }
+    if (*isl_count != count) {
+        mx_printerr("error: invalid number of islands\n");
+        exit (-1);
+    }
+    res = malloc(sizeof(char *) * count);
+        mx_print_strarr(buf, "__");
+    for (int i = 0, j = 0; buf[j] != NULL; j++) {
+        if (mx_strcmp(buf[j], "del") != 0) {
+            res[i] = mx_strdup(buf[j]);
+            i++;
+        }
+    }
+    res[count] = NULL;
+    // mx_del_strarr(&buf);
+        printf("%d\n", *isl_count);
+
+        mx_print_strarr(res, "\n");
+ 
+    return res;
 }
+
+// || 
