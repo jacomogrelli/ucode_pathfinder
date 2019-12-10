cfc26988f5ce431b71f08877e1f85fee0c7f7120
Oleksii Nechaiev <onechaiev@e2r4p7.unit.ua>
2019-12-10 18:01:13 +0200

    -

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "pathfinder.h"
 
+static void pf_cl(char **mat, char **uniq_mat, int **adj_mat, int **rout_mat);
+
 int main (int argc, char **argv) {
     char **mat = NULL;
     char **uniq_mat = NULL;
@@ -12,10 +14,22 @@ int main (int argc, char **argv) {
     adj_mat = mx_pf_adjmat(mat, uniq_mat, isl_count);
     rout_mat = mx_pf_pink_floyd(adj_mat, isl_count);
     mx_pf_output(uniq_mat, adj_mat, rout_mat);
+    pf_cl(mat, uniq_mat, adj_mat, rout_mat);
+}
 
+static void pf_cl(char **mat, char **uniq_mat, int **adj_mat, int **rout_mat) {
+    int count = 0;
+
+    while (uniq_mat[count])
+        count++;    
     mx_del_strarr(&mat);
     mx_del_strarr(&uniq_mat);
+    for (int i = 0; i < count; i++) {
+        free(adj_mat[i]);
+    }
     free(adj_mat);
+    for (int i = 0; i < count; i++) {
+        free(rout_mat[i]);
+    }
     free(rout_mat);
-    // system ("leaks -q pathfinder");
 }
--- src/mx_pf_print.c
+++ src/mx_pf_print.c
@@ -2,17 +2,16 @@
 
 static void pf_print_path(char **uniq_mat, int *stack, int len);
 static void pf_print_route(char **uniq_mat, int *stack, int len);
-static void pf_print_dist(int **adj_mat, char **uniq_mat, int *stack, int len);
+static void pf_print_dist(int **adj_mat, int *stack, int len);
 
-void mx_pf_print(char **uniq_mat, int **adj_mat, int *stack)
-{
+void mx_pf_print(char **uniq_mat, int **adj_mat, int *stack) {
     int len = 0;
 
     while (stack[len] != -2)
         len++;
     pf_print_path(uniq_mat, stack, len);
     pf_print_route(uniq_mat, stack, len);
-    pf_print_dist(adj_mat, uniq_mat, stack, len);
+    pf_print_dist(adj_mat, stack, len);
 }
 
 static void pf_print_path(char **uniq_mat, int *stack, int len) {
@@ -31,8 +30,7 @@ static void pf_print_route(char **uniq_mat, int *stack, int len) {
     mx_printstr(uniq_mat[stack[0]]);
 }
 
-static void pf_print_dist(int **adj_mat, char **uniq_mat, int *stack, int len)
-{
+static void pf_print_dist(int **adj_mat, int *stack, int len) {
     mx_printstr("\nDistance: ");
     if (len - 2 != 0) {
         for (int i = len - 1; i > 0; i--) {
